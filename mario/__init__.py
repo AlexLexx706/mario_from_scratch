e4cc4f6949a75f9b460df67f7e08bafa97956ab7
alex <akalmykov@topcon.com>
2020-05-14 02:17:46 +0300

    update

--- mario/__init__.py
+++ mario/__init__.py
@@ -10,8 +10,50 @@ import logging
 LOG = logging.getLogger(__name__)
 
 
+class Box:
+    def __init__(self, scene, pos, size):
+        self.pos = pos
+        self.size = size
+        self.scene = scene
 
-class SceneWidget(QtWidgets.QWidget):
+    def update(self, painter):
+        pass
+
+    def rect(self):
+        return QtCore.QRectF(
+            self.pos[0], self.pos[1],
+            self.size[0], self.size[1])
+
+
+class Block(Box):
+    block_1 = QtCore.QRect(0, 0, 16, 16)
+    block_2 = QtCore.QRect(16, 0, 16, 16)
+
+    def __init__(self, scene, src_rect, pos, size):
+        super().__init__(scene, pos, size)
+
+        # create brush if not exist
+        src_rect_hash = str(src_rect)
+        if src_rect_hash not in self.scene.blocks_brushs_map:
+            self.scene.blocks_brushs_map[src_rect_hash] = QtGui.QBrush(
+                self.scene.blocks_pixmap.copy(src_rect))
+
+        self.brush = self.scene.blocks_brushs_map[src_rect_hash]
+        self.transform = QtGui.QTransform()
+        self.transform.translate(pos[0], pos[1])
+
+    def update(self, painter):
+        self.brush.setTransform(self.transform)
+        painter.setBrush(self.brush)
+        painter.setPen(Qt.darkCyan)
+        painter.drawRect(self.rect())
+        # painter.drawPixmap(
+        #     self.rect(),
+        #     self.scene.blocks_pixmap,
+        #     self.src_rect)
+
+
+class Mario(Box):
     left_jump = QtCore.QRectF(29, 0, 18, 16)
     left_stop = QtCore.QRectF(58, 0, 18, 16)
     left_start = QtCore.QRectF(88, 0, 18, 16)
@@ -29,14 +71,8 @@ class SceneWidget(QtWidgets.QWidget):
     fail_left = QtCore.QRectF(0, 15, 15, 16)
     fail_right = QtCore.QRectF(390, 15, 15, 16)
 
-    all_steps = [
-        left_jump, left_stop, left_start, left_walk_1, left_walk_2, left_stay,
-        right_stay, right_walk_2, right_walk_1, right_start, right_stop, right_jump,
-        fail_left, fail_right
-    ]
-
-    left_walk_seq = [left_start, left_walk_1, left_walk_2] 
-    right_walk_seq = [right_start, right_walk_1, right_walk_2] 
+    left_walk_seq = [left_start, left_walk_1, left_walk_2]
+    right_walk_seq = [right_start, right_walk_1, right_walk_2]
 
     walk_animation_speed = 0.1
 
@@ -48,25 +84,109 @@ class SceneWidget(QtWidgets.QWidget):
         JUMP_LEFT = 5
         JUMP_RIGHT = 6
 
+    def __init__(self, scene, pos):
+        super().__init__(scene, pos, [40, 40])
+        self.state = self.States.WAIT_LEFT
+        self.start_time = time.time()
+        self.src_rect = self.left_stay
+
+    def update(self, painter):
+        target = self.rect()
+
+        # update states
+        if self.state == self.States.WAIT_LEFT:
+            self.src_rect = self.left_stay
+            self.check_start_move()
+        elif self.state == self.States.WAIT_RIGHT:
+            self.src_rect = self.right_stay
+            self.check_start_move()
+        elif self.state == self.States.RUN_LEFT:
+            if not self.scene.key_map.get(Qt.Key_Left, 0):
+                self.state = self.States.WAIT_LEFT
+                self.src_rect = self.left_stay
+            else:
+                self.src_rect = self.left_walk_seq[int(
+                    (time.time() - self.start_time) /
+                    self.walk_animation_speed) % len(self.left_walk_seq)]
+        elif self.state == self.States.RUN_RIGHT:
+            if not self.scene.key_map.get(Qt.Key_Right, 0):
+                self.state = self.States.WAIT_RIGHT
+                self.src_rect = self.right_stay
+            else:
+                self.src_rect = self.right_walk_seq[int(
+                    (time.time() - self.start_time) /
+                    self.walk_animation_speed) % len(self.right_walk_seq)]
+        elif self.state == self.States.JUMP_LEFT:
+            pass
+        elif self.state == self.States.JUMP_RIGHT:
+            pass
+
+        painter.drawPixmap(
+            target,
+            self.scene.mario_pixmap,
+            self.src_rect)
+
+        painter.drawRect(target)
+
+    def check_start_move(self):
+        # start move left
+        if self.scene.key_map.get(Qt.Key_Left, 0):
+            self.state = self.States.RUN_LEFT
+            self.src_rect = self.left_start
+            self.start_time = time.time()
+        # start move right
+        elif self.scene.key_map.get(Qt.Key_Right, 0):
+            self.state = self.States.RUN_RIGHT
+            self.src_rect = self.right_start
+            self.start_time = time.time()
+
+
+class Scene:
+    def __init__(self):
+        self.items = []
+        self.key_map = {}
+        self.blocks_brushs_map = {}
+        file_path = os.path.join(
+            os.path.split(__file__)[0], 'images/mario.png')
+        self.mario_pixmap = QtGui.QPixmap(file_path)
+
+        if self.mario_pixmap.isNull():
+            LOG.error("Cannot load pixmap:%s" % (file_path))
+
+        file_path = os.path.join(
+            os.path.split(__file__)[0], 'images/blocks.png')
+        self.blocks_pixmap = QtGui.QPixmap(file_path)
+
+        if self.blocks_pixmap.isNull():
+            LOG.error("Cannot load pixmap:%s" % (file_path))
+
+    def update(self, painter):
+        for item in self.items:
+            item.update(painter)
+
+
+class SceneWidget(QtWidgets.QWidget):
+
     def __init__(self, parent=None):
         super(SceneWidget, self).__init__(parent)
         self.timer = QtCore.QTimer(self)
         self.timer.timeout.connect(self.next_animation_frame)
         self.timer.start(50)
+        self.start_time = time.time()
         self.frame_no = 0
         self.antialiased = True
-        self.key_map = {}
-        self.state = self.States.WAIT_LEFT
-        self.start_time = time.time()
-        self.src_rect = self.left_stay
 
-        # load character texrure
-        file_path = os.path.join(
-            os.path.split(__file__)[0], 'images/mario.png')
-        self.pixmap_characters = QtGui.QPixmap(file_path)
+        # create scene and mario
+        self.scene = Scene()
 
-        if self.pixmap_characters.isNull():
-            LOG.warning('cult not load texture:%s' % (file_path, ))
+        mario = Mario(self.scene, [0.0, -40])
+        self.scene.items.append(mario)
+
+        block = Block(self.scene, Block.block_1, [-1000, 0], [2000, 32])
+        self.scene.items.append(block)
+
+        block = Block(self.scene, Block.block_2, [-1000, 32], [2000, 32])
+        self.scene.items.append(block)
 
         self.setBackgroundRole(QtGui.QPalette.Base)
         self.setSizePolicy(
@@ -87,71 +207,14 @@ class SceneWidget(QtWidgets.QWidget):
         painter.setFont(QtGui.QFont("Arial", 30))
         painter.drawText(0, 30, "frame:%d time:%3.3f" % (
             self.frame_no, time.time() - self.start_time))
-
         painter.translate(self.width() / 2, self.height() / 2)
-        target = QtCore.QRectF(0.0, 0.0, 100.0, 100.0)
-
-        # update states
-        if self.state == self.States.WAIT_LEFT:
-            self.src_rect = self.left_stay
-            self.check_start_move()
-        elif self.state == self.States.WAIT_RIGHT:
-            self.src_rect = self.right_stay
-            self.check_start_move()
-        elif self.state == self.States.RUN_LEFT:
-            if not self.key_map.get(Qt.Key_Left, 0):
-                self.state = self.States.WAIT_LEFT
-                self.src_rect = self.left_stay
-            else:
-                self.src_rect = self.left_walk_seq[int(
-                    (time.time() - self.start_time) /
-                    self.walk_animation_speed) % len(self.left_walk_seq)]
-        elif self.state == self.States.RUN_RIGHT:
-            if not self.key_map.get(Qt.Key_Right, 0):
-                self.state = self.States.WAIT_RIGHT
-                self.src_rect = self.right_stay
-            else:
-                self.src_rect = self.right_walk_seq[int(
-                    (time.time() - self.start_time) /
-                    self.walk_animation_speed) % len(self.right_walk_seq)]
-        elif self.state == self.States.JUMP_LEFT:
-            pass
-        elif self.state == self.States.JUMP_RIGHT:
-            pass
-
-        painter.drawPixmap(
-            target,
-            self.pixmap_characters,
-            # self.all_steps[self.frame_no % len(self.all_steps)]
-            self.src_rect)
-
-        painter.drawRect(target)
-
-    def check_start_move(self):
-        # start move left
-        if self.key_map.get(Qt.Key_Left, 0):
-            self.state = self.States.RUN_LEFT
-            self.src_rect = self.left_start
-            self.start_time = time.time()
-        # start move right
-        elif self.key_map.get(Qt.Key_Right, 0):
-            self.state = self.States.RUN_RIGHT
-            self.src_rect = self.right_start
-            self.start_time = time.time()
+        self.scene.update(painter)
 
     def keyPressEvent(self, event):
-        self.key_map[event.key()] = 1
-        if event.key() == Qt.Key_Up:
-            print('up')
-        elif event.key() == Qt.Key_Down:
-            print('down')
-        elif event.key() == Qt.Key_Left:
-            print('left')
-        elif event.key() == Qt.Key_Right:
-            print('right')
+        self.scene.key_map[event.key()] = 1
 
     def keyReleaseEvent(self, event):
-        self.key_map[event.key()] = 0
+        self.scene.key_map[event.key()] = 0
 
 
 if __name__ == '__main__':
