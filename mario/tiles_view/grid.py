f66ac0a7f578d75eee97a2dfb1f9bb45db3c19a2
alex <akalmykov@topcon.com>
2020-06-02 00:21:47 +0300

    update

--- mario/tiles_view/grid.py
+++ mario/tiles_view/grid.py
@@ -4,16 +4,22 @@ import math
 
 
 class Grid:
+    selected_size = 4
+    not_selected_size = 2
+
     brush = QtGui.QBrush(Qt.NoBrush)
+
     pen = QtGui.QPen(Qt.SolidLine)
     pen.setColor(Qt.red)
     pen.setCosmetic(True)
-    pen.setWidth(2)
+    pen.setWidth(not_selected_size)
 
     pen_blue = QtGui.QPen(Qt.SolidLine)
     pen_blue.setColor(Qt.blue)
     pen_blue.setCosmetic(True)
     pen_blue.setWidth(2)
+
+    selector_brush = QtGui.QBrush(Qt.blue, Qt.SolidPattern)
 
     ide_state = 0
     move_state = 1
@@ -30,8 +36,12 @@ class Grid:
         self.state = self.ide_state
         self.rows = 3
         self.columns = 3
-        self.columns_offset = 1
-        self.rows_offset = 1
+        self.columns_offset = 0
+        self.rows_offset = 0
+        self.selected = 1
+        self.show_grid = 1
+        self.selector_offsets = None
+        self.show_selector = 1
 
     def set_rows(self, rows):
         self.rows = rows
@@ -45,10 +55,34 @@ class Grid:
     def set_rows_offset(self, offset):
         self.rows_offset = offset
 
-    def update(self, painter):
+    def draw_selecter(self, painter):
+        painter.setBrush(self.selector_brush)
+        painter.setPen(Qt.NoPen)
+        pos = None
+        if self.state == self.move_top_left:
+            pos = self.rect.topLeft()
+        elif self.state == self.move_top_right:
+            pos = self.rect.topRight() - QtCore.QPointF(
+                self.selector_offsets.x(), 0)
+        elif self.state == self.move_bottom_left:
+            pos = self.rect.bottomLeft() - QtCore.QPointF(
+                0., self.selector_offsets.y())
+        elif self.state == self.move_bottom_right:
+            pos = self.rect.bottomRight() - QtCore.QPointF(
+                self.selector_offsets.x(), self.selector_offsets.y())
+        if pos is not None:
+            rect = QtCore.QRectF(
+                pos.x(),
+                pos.y(),
+                self.selector_offsets.x(),
+                self.selector_offsets.y())
+            print(rect)
+            painter.drawRect(rect)
+
+    def draw_grid(self, painter):
         painter.setBrush(self.brush)
+        self.pen.setWidth(self.not_selected_size)
         painter.setPen(self.pen)
-        painter.drawRect(self.rect)
 
         s_x = self.rect.x()
         s_y = self.rect.y()
@@ -63,10 +97,7 @@ class Grid:
             for i in range(1, self.rows + 1):
                 y = s_y + i * height
                 y_2 = y - self.rows_offset
-                # painter.setPen(self.pen_blue)
                 painter.drawLine(s_x, y, e_x, y)
-
-                # painter.setPen(self.pen)
                 painter.drawLine(s_x, y_2, e_x, y_2)
         else:
             for i in range(1, self.rows + 1):
@@ -79,85 +110,104 @@ class Grid:
             for i in range(1, self.columns + 1):
                 x = s_x + i * width
                 x_2 = x - self.columns_offset
-                # painter.setPen(self.pen_blue)
                 painter.drawLine(x, s_y, x, e_y)
-
-                # painter.setPen(self.pen)
                 painter.drawLine(x_2, s_y, x_2, e_y)
         else:
             for i in range(1, self.columns + 1):
                 x = s_x + i * width
                 painter.drawLine(x, s_y, x, e_y)
 
+    def update(self, painter):
+        painter.setBrush(self.brush)
+        self.pen.setWidth(
+            self.selected_size if self.selected else self.not_selected_size)
+        painter.setPen(self.pen)
+        painter.drawRect(self.rect)
+
+        if self.show_grid:
+            self.draw_grid(painter)
+
+        if self.show_selector:
+            self.draw_selecter(painter)
+
     def start_move(self, pos):
-        top_left = self.rect.topLeft()
-        # 1. calculate offset for choose borders
-        offsets = self.view.camera.to_scene(
-            self.view.camera.to_view(top_left) +
-            QtCore.QPointF(
-                self.selector_size,
-                self.selector_size)) - top_left
+        try:
+            if self.rect.contains(pos):
+                top_left = self.rect.topLeft()
+                # 1. calculate offset for choose borders
+                self.selector_offsets = self.view.camera.to_scene(
+                    self.view.camera.to_view(top_left) +
+                    QtCore.QPointF(
+                        self.selector_size,
+                        self.selector_size)) - top_left
 
-        if self.rect.contains(pos):
-            top_left_rect = QtCore.QRectF(
-                top_left.x(),
-                top_left.y(),
-                offsets.x(),
-                offsets.y())
-
-            if top_left_rect.contains(pos):
-                self.state = self.move_top_left
-                self.start_move_pos = pos
-                self.top_left = self.rect.topLeft()
-                return 1
-            else:
-                top_right = self.rect.topRight()
-                top_right_rect = QtCore.QRectF(
-                    top_right.x() - offsets.x(),
+                top_left_rect = QtCore.QRectF(
+                    top_left.x(),
                     top_left.y(),
-                    offsets.x(),
-                    offsets.y())
-
-                if top_right_rect.contains(pos):
-                    self.state = self.move_top_right
+                    self.selector_offsets.x(),
+                    self.selector_offsets.y())
+                if top_left_rect.contains(pos):
+                    self.state = self.move_top_left
                     self.start_move_pos = pos
-                    self.top_right = self.rect.topRight()
+                    self.top_left = self.rect.topLeft()
                     return 1
                 else:
-                    bottom_left = self.rect.bottomLeft()
-                    bottom_left_rect = QtCore.QRectF(
-                        bottom_left.x(),
-                        bottom_left.y() - offsets.x(),
-                        offsets.x(),
-                        offsets.y())
+                    top_right = self.rect.topRight()
+                    top_right_rect = QtCore.QRectF(
+                        top_right.x() - self.selector_offsets.x(),
+                        top_left.y(),
+                        self.selector_offsets.x(),
+                        self.selector_offsets.y())
 
-                    if bottom_left_rect.contains(pos):
-                        self.state = self.move_bottom_left
+                    if top_right_rect.contains(pos):
+                        self.state = self.move_top_right
                         self.start_move_pos = pos
-                        self.bottom_left = self.rect.bottomLeft()
+                        self.top_right = self.rect.topRight()
                         return 1
                     else:
-                        bottom_right = self.rect.bottomRight()
-                        bottom_right_rect = QtCore.QRectF(
-                            bottom_right.x() - offsets.x(),
-                            bottom_right.y() - offsets.x(),
-                            offsets.x(),
-                            offsets.y())
+                        bottom_left = self.rect.bottomLeft()
+                        bottom_left_rect = QtCore.QRectF(
+                            bottom_left.x(),
+                            bottom_left.y() - self.selector_offsets.x(),
+                            self.selector_offsets.x(),
+                            self.selector_offsets.y())
 
-                        if bottom_right_rect.contains(pos):
-                            self.state = self.move_bottom_right
+                        if bottom_left_rect.contains(pos):
+                            self.state = self.move_bottom_left
                             self.start_move_pos = pos
-                            self.bottom_right = self.rect.bottomRight()
+                            self.bottom_left = self.rect.bottomLeft()
                             return 1
                         else:
-                            print("all")
-                            self.state = self.move_state
-                            self.start_move_pos = pos
-                            self.top_left = self.rect.topLeft()
-                            return 1
+                            bottom_right = self.rect.bottomRight()
+                            bottom_right_rect = QtCore.QRectF(
+                                bottom_right.x() - self.selector_offsets.x(),
+                                bottom_right.y() - self.selector_offsets.x(),
+                                self.selector_offsets.x(),
+                                self.selector_offsets.y())
+
+                            if bottom_right_rect.contains(pos):
+                                self.state = self.move_bottom_right
+                                self.start_move_pos = pos
+                                self.bottom_right = self.rect.bottomRight()
+                                return 1
+                            else:
+                                self.state = self.move_state
+                                self.start_move_pos = pos
+                                self.top_left = self.rect.topLeft()
+                                return 1
+        finally:
+            self.view.update()
+
+    def set_selected(self, selected):
+        self.selected = selected
+        self.view.update()
 
     def stop_move(self):
         self.state = self.ide_state
+        self.selector_offsets = None
+        self.rect = self.rect.normalized()
+        self.view.update()
+
 
     def move(self, pos):
         if self.state == self.move_state:
